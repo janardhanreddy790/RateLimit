43674520e812ee4ac3614114fdac0d30b15ce85f
Janardhan Reddy Nagam <nagamjanardhanredy@gmail.com>
2025-07-04 17:49:43 +0200

    Remove unwanted config

--- src/main/java/com/nagam/example/ratelimiter/config/RedisConfig.java
+++
@@ -1,8 +0,0 @@
-package com.nagam.example.ratelimiter.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RedisConfig {
-    // Uses Spring Boot auto-configuration for Redis
-}
